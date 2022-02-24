b2b71f2357c2a11150ec89158692da1726657841
FlorenciaCanteros <flor.nicole16@gmail.com>
2022-02-24 11:54:53 -0300

    agrego ultimos cambios

--- src/middlewares/paraEditUser.js
+++
@@ -1,8 +0,0 @@
-function paraEditUser(req, res, next) {
-    if (req.session.userLogged) {
-        return res.redirect('/editarusuario/:id');
-    }
-    next();
-}
-
-module.exports = paraEditUser;
--- src/routers/UsersRutas.js
+++ src/routers/UsersRutas.js
@@ -1,6 +1,5 @@
 const express= require ('express');
 const router= express.Router();
-
 const path = require('path');
 
 
@@ -10,7 +9,7 @@ const {validaciones} =require('../middlewares/validatorMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const userLoggedMiddleware = require('../middlewares/userLoggedMiddleware');
-const paraEditUser = require('../middlewares/paraEditUser');
+
 
 //controllers
 let usersController= require('../controllers/usersController');
@@ -41,8 +40,6 @@ router.get('/listar', guestMiddleware ,usersController.listar);
 router.post('/register', uploadFile.single('avatar'), validaciones, usersController.processRegister);//agrega usuarios
 router.get('/borrar/:id', guestMiddleware ,usersController.delete);
 
-//router.get('/editarusuario/:id',usersController.edit);//este misdelware te deja entrar si tas registrada
-//router.put('/editarusuario/:id',usersController.update);
 router.get('/perfil/edit/:id', usersController.edit)//detalle
 router.put('/perfil/edit/:id', uploadFile.single('avatar'),usersController.update)
 
